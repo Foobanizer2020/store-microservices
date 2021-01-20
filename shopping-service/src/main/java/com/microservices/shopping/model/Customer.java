e271ac19e660e5d6b61f505128b39a4ab58f4d9e
Raúl González <gonzalv@ciencias.unam.mx>
2021-01-19 21:58:26 -0600

    Configura el repositorio para el config-service
    Corrige un error con la etiqueta Builder y Feign
    
    .
    .
    .
    
    más de una hora buscando cuál era el error >:v

--- shopping-service/src/main/java/com/microservices/shopping/model/Customer.java
+++ shopping-service/src/main/java/com/microservices/shopping/model/Customer.java
@@ -1,10 +1,14 @@
 package com.microservices.shopping.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Customer {
 	private Long id;
     private String numberID;
