d73e081288fbbd341a7a7760fa34b3f817431516
Raúl González <gonzalv@ciencias.unam.mx>
2021-01-20 16:23:47 -0600

    Agrega configuraciones para que se ejecute payment-service

--- payment-service/src/main/java/com/microservices/payment/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/microservices/payment/PaymentServiceApplication.java
@@ -1,16 +1,10 @@
 package com.microservices.payment;
 
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
-@EnableHystrix
-@EnableHystrixDashboard
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 @EnableEurekaClient
 @SpringBootApplication
 public class PaymentServiceApplication {
