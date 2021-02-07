14c7359ca29a8d9506a49fea133263864482750c
Raúl González <gonzalv@ciencias.unam.mx>
2021-02-07 10:46:55 -0600

    Corrige pequeño error con feign en customerservice

--- customer-service/src/main/java/com/microservices/customer/client/PaymentClient.java
+++ customer-service/src/main/java/com/microservices/customer/client/PaymentClient.java
@@ -2,6 +2,7 @@
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,7 +12,7 @@ import com.microservices.customer.model.*;
 @FeignClient(name="payment-service")
 @RequestMapping(value = "/cards")
 public interface PaymentClient {
-	
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<Card> getCard(@PathVariable("id") Long id);
 	
 }
